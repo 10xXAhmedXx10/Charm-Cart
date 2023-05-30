392ac8f18641a8517f88d7f3fd4ffbd30352fcd7
Ahmed <ahmed.salihi9610@gmail.com>
2023-05-29 22:37:10 -0600

    Working on fixing the nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./components/Home"
-import Account from './components/Account';
-import Womenfashion from './components/pages/Womenfashion';
+import Home from "./pages/Home"
+import Account from './pages/Account';
+import Womenfashion from './pages/Womenfashion';
+import Books from "./pages/Books"
+import Clinic from "./pages/Clinic"
+import Health from "./pages/Health"
+import Movies from "./pages/Movies"
+import Beauty from "./pages/Beauty"
+import Music from "./pages/Music"
+import Smartphone from "./pages/Smartphones"
+import Menfashion from "./pages/Menfashion"
+import Kidsfashion from "./pages/Smartphones"
+import Tablets from "./pages/Tablets"
+
 
 import Navbar from './components/Navbar';
 import "./App.css"
@@ -11,11 +22,25 @@ function App() {
     return (
         <Router>
           <Navbar />
+            
             <Routes>
+
+                <Route path="/home" element={<Home />} /> 
                 <Route path="/womenfashion" element={<Womenfashion />} />
+                <Route path="/beauty" element={<Beauty />} />
+                <Route path="/clinic" element={<Clinic />} />
+                <Route path="/health" element={<Health />} />
+                <Route path="/Kidsfashion" element={<Kidsfashion />} />
+                <Route path="/menfashion" element={<Menfashion />} />
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/music" element={<Music />} />
+                <Route path="/smartphones" element={<Smartphone />} />
+                <Route path="/books" element={<Books />} />
+                <Route path="/tablets" element={<Tablets />} />
                 <Route path="/account" element={<Account />} />
             </Routes>
         </Router>
+        
     );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,33 @@ import "../App.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <img className='logo' src='/assets/images/Charm.svg' alt='logo' />
+      <NavLink exact to="/clinic" activeClassName="active" className="nav-link">
+      Clinic
+      </NavLink>
+      <NavLink exact to="/health" activeClassName="active" className="nav-link">
+      Health
+      </NavLink>
       <NavLink exact to="/womenfashion" activeClassName="active" className="nav-link">
-      womenfashion
+      Women Fashion
+      </NavLink>
+      <NavLink exact to="/menfashion" activeClassName="active" className="nav-link">
+      Men Fashion
+      </NavLink>
+      <NavLink exact to="/kidsfashion" activeClassName="active" className="nav-link">
+      Kids Fashion
+      </NavLink>
+      <NavLink exact to="/movies" activeClassName="active" className="nav-link">
+      Movies
+      </NavLink>
+      <NavLink exact to="/music" activeClassName="active" className="nav-link">
+      Music
+      </NavLink>
+      <NavLink exact to="/smartphones" activeClassName="active" className="nav-link">
+      Smart Phones
+      </NavLink>
+      <NavLink exact to="/tablets" activeClassName="active" className="nav-link">
+      Tablets
       </NavLink>
       <NavLink to="/account" activeClassName="active" className="nav-link">
         Account
